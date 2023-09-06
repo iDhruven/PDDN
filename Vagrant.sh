12037a8defb6b1cf987cb39d084f5d784b7e2cfe
iDhruven <35227460+iDhruven@users.noreply.github.com>
2023-09-06 01:38:42 -0700

    Create Vagrant.sh

---
+++ Vagrant.sh
@@ -0,0 +1,14 @@
+# Navigate to the workspace directory
+cd $WORKSPACE
+
+# Download the Vagrant installer
+wget https://releases.hashicorp.com/vagrant/X.Y.Z/vagrant_X.Y.Z_linux_amd64.zip
+
+# Unzip the installer
+unzip vagrant_X.Y.Z_linux_amd64.zip
+
+# Make Vagrant executable
+chmod +x vagrant
+
+# Optionally, move it to a directory included in the PATH
+mv vagrant /usr/local/bin/
