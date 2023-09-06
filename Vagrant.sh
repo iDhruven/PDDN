c59fe26751770f430175b7a39f691e181bbad08f
iDhruven <35227460+iDhruven@users.noreply.github.com>
2023-09-06 01:52:53 -0700

    Update Vagrant.sh

--- Vagrant.sh
+++ Vagrant.sh
@@ -5,10 +5,10 @@ cd $WORKSPACE
 curl -O https://releases.hashicorp.com/vagrant/2.3.7/vagrant_2.3.7_linux_amd64.zip
 
 # Unzip the installer
-unzip vagrant_X.Y.Z_linux_amd64.zip
+unzip vagrant_2.3.7_linux_amd64.zip
 
 # Make Vagrant executable
 chmod +x vagrant
 
 # Optionally, move it to a directory included in the PATH
-mv vagrant /usr/local/bin/
+#mv vagrant /usr/local/bin/
